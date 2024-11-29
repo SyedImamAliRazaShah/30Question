d89e9306e573f416d1e634b9266e299fc3da9e30
SyedImamAliRazaShah <sayedalirazashah985@gmail.com>
2024-11-29 15:37:41 +0500

    questions

---
+++ 30Q.cs
@@ -0,0 +1,372 @@
+
+                        //1st Negative/positve
+//Console.Write("number");
+//Console.Write("Enter your number: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//if (num > 0)
+//{
+//    Console.WriteLine("Positive");
+//}
+//else if (num == 0) 
+//{
+//    Console.WriteLine("its Zero");
+//}
+//else
+//{
+//    Console.WriteLine("Negative");
+//}
+
+                //2nd voat eligibility
+
+//Console.Write("Enter Your age: ");
+//int voat = Convert.ToInt32(Console.ReadLine());
+//if (voat >= 18)
+//{
+//    Console.WriteLine("You are eligibale for voat");
+//}
+//else
+//{
+//    Console.WriteLine("You are not eligibale for voat");
+//}
+
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Transactions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //3rd Calculator
+        //Console.WriteLine("Calculator");
+        //Console.Write("Enter 1st number: ");
+        //int num1 = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Enter 2ND number: ");
+        //int num2 = Convert.ToInt32(Console.ReadLine());
+        //int pls =num1 + num2;
+        //int mns=num1 - num2;
+        //int mul = num1 * num2;
+        //int div = num1 / num2;
+        //Console.WriteLine("ADDITION: "+pls+" ;");
+        //Console.WriteLine("SUTRACTION: " + mns+" ;");
+        //Console.WriteLine("MULTIPLICATION: " + mul+" ;");
+        //Console.WriteLine("DIVISION: " + div+" ;");
+
+
+        //4th largest in three number
+
+        //Console.WriteLine("Find Largest number");
+        //Console.Write("Enter 1st number: ");
+        //int num1 = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Enter 2ND number: ");
+        //int num2 = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Enter 1st number: ");
+        //int num3 = Convert.ToInt32(Console.ReadLine());
+        //if (num1 >= num2)
+        //{
+        //    Console.WriteLine("Num1 is large");
+        //}
+        //else if (num2 >= num1 && num3 <= num2)
+        //{
+        //    Console.WriteLine("Num2 is large");
+        //}
+        //else if (num3 >= num2)
+        //{
+        //    Console.WriteLine("num3 is large");
+        //}
+
+        //5th Days of Week
+
+        //Console.Write("Enter Number to See week days: ");
+        //int week=Convert.ToInt32(Console.ReadLine());
+        //if (week == 1)
+        //{
+        //    Console.WriteLine("Saturday");
+        //}else if (week == 2)
+        //{
+        //    Console.WriteLine("Sunday");
+        //}else if (week == 3)
+        //{
+        //    Console.WriteLine("Monday");
+        //}else if (week == 4)
+        //{
+        //    Console.WriteLine("Tuesday");
+        //}
+        //else if (week == 5)
+        //{
+        //    Console.WriteLine("Wednesday");
+        //}
+        //else if (week == 6)
+        //{
+        //    Console.WriteLine("Thrusday");
+        //}
+        //else if (week == 7)
+        //{
+        //    Console.WriteLine("Friday");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Bas kar Bhai");
+        //}
+
+        //6th Month name
+
+        //Console.Write("Enter Number to See MOnth Name: ");
+        //int Month = Convert.ToInt32(Console.ReadLine());
+        //if (Month == 1)
+        //{
+        //    Console.WriteLine("January");
+        //}
+        //else if (Month == 2)
+        //{
+        //    Console.WriteLine("Febrary");
+        //}
+        //else if (Month == 3)
+        //{
+        //    Console.WriteLine("March");
+        //}
+        //else if (Month == 4)
+        //{
+        //    Console.WriteLine("Aprail");
+        //}
+        //else if (Month == 5)
+        //{
+        //    Console.WriteLine("May");
+        //}
+        //else if (Month == 6)
+        //{
+        //    Console.WriteLine("Jun");
+        //}
+        //else if (Month == 7)
+        //{
+        //    Console.WriteLine("July");
+        //}
+
+        //else if (Month == 8)
+        //{
+        //    Console.WriteLine("August");
+        //}
+        //else if (Month == 9)
+        //{
+        //    Console.WriteLine("September");
+        //}
+        //else if (Month == 10)
+        //{
+        //    Console.WriteLine("Octubar");
+        //}
+        //else if (Month == 11)
+        //{
+        //    Console.WriteLine("Novembar");
+        //}
+        //else if (Month == 12)
+        //{
+        //    Console.WriteLine("December");
+        //}
+
+        //7th Check of Divionability
+
+        //Console.WriteLine("Check Divisionable number");
+        //Console.Write("Enter 1st Number to chek: ");
+        //int div1 = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Enter 1st Number to chek: ");
+        //int div2 = Convert.ToInt32(Console.ReadLine());
+        //int num=div1 / div2;
+        //if (num <= 0)
+        //{
+        //    Console.WriteLine("It is Not Divisionbale " );
+        //}
+        //else {
+        //    Console.WriteLine("It is  Divisionbale " );
+
+        //}
+
+
+        //emp bonus cal
+
+        //Console.Write("Enter Year of experiance: ");
+        //int num1 = Convert.ToInt32(Console.ReadLine());
+
+        //if (num1 >= 5)
+        //{
+        //    Console.Write("Enter your Salary: ");
+        //    int num2 = Convert.ToInt32(Console.ReadLine());
+        //    int num22 = num2 * 10 / 100;
+        //    Console.WriteLine("Your Actual Salary is: "+num2);
+        //    Console.WriteLine("Your Bonus is: "+num22);
+        //}else if (num1 <= 5)
+        //{
+        //    Console.Write("Enter your Salary: ");
+        //    int num2 = Convert.ToInt32(Console.ReadLine());
+        //    int num22 = num2 * 2 / 100;
+        //    Console.WriteLine("Your Actual Salary is: " + num2);
+        //    Console.WriteLine("Your Bonus is: " + num22);
+        //}
+
+
+
+        // Small in three number
+
+        //Console.WriteLine("Find Smallest number");
+        //Console.Write("Enter 1st number: ");
+        //int num1 = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Enter 2ND number: ");
+        //int num2 = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Enter 3rd number: ");
+        //int num3 = Convert.ToInt32(Console.ReadLine());
+        //if (num1 <= num2)
+        //{
+        //    Console.WriteLine("Small Number is" + num1);
+        //}
+        //else if (num2 <= num1 && num3 >= num2)
+        //{
+        //    Console.WriteLine("Small Number is " + num2);
+        //}
+        //else if (num3 <= num2)
+        //{
+        //    Console.WriteLine("Small Number is: " + num3);
+        //}
+
+
+        //Simple Corency Converter
+
+        //Console.Write("Convert Currency: ");
+        //int cor = Convert.ToInt32(Console.ReadLine());
+
+        //int usd = cor * 268;
+        //Console.WriteLine("US Dollor = "+usd);
+        //int ind = cor * 2;
+        //Console.WriteLine("Ind Currency = "+ind);
+
+        // Electric Bill
+
+        //Console.WriteLine("Check Electricity Bill");
+        //Console.Write("Enter Bill Unit: ");
+        //int cor = Convert.ToInt32(Console.ReadLine());
+        //int bill = cor * 50;
+        //Console.WriteLine("Your Current Bill is: "+bill);
+        //Console.WriteLine("Per Unit Rs 50.");
+
+        //greater of two number
+
+        //Console.WriteLine("Find Greater number");
+        //Console.Write("Enter 1st number: ");
+        //int num1 = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Enter 2ND number: ");
+        //int num2 = Convert.ToInt32(Console.ReadLine());
+        //if (num1 >= num2)
+        //{
+        //    Console.WriteLine(num1+ ": Is Greater Then: " + num2);
+        //}
+        //else if (num2 >= num1 )
+        //{
+        //    Console.WriteLine(num2+ ": Is Greater Then: " +num1);
+        //}
+
+        //Leap Year Check
+
+        //Console.Write("Check Leap Year: ");
+        //int leap = Convert.ToInt32(Console.ReadLine());
+        //if (leap % 4 == 0)
+        //{
+        //    Console.WriteLine("It is Leap Year");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Its is not leap yaer");
+        //}
+
+        //Even Odd
+
+        //Console.Write("Check Even/Odd: ");
+        //int leap = Convert.ToInt32(Console.ReadLine());
+        //if (leap % 2 == 0)
+        //{
+        //    Console.WriteLine("It is Even Number");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("It is  ODD Number");
+        //}
+
+
+        // Grade calculation
+
+        //Console.Write("Enter Your English Marks: ");
+        //int num = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Enter Your Urdu Marks: ");
+        //int num1 = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Enter Your Math Marks: ");
+        //int num2 = Convert.ToInt32(Console.ReadLine());
+        //int total =num+num1+num2;
+        //int obt = 300;
+        //int per = (total*100)/obt;
+        //if (per <= 100 && per >= 90)
+        //{
+        //    Console.WriteLine("Total Marks: " + total);
+        //    Console.WriteLine("Obtained Marks: "+obt);
+        //    Console.WriteLine("Grade A+");
+        //    Console.WriteLine("You Persentage is "+per+"%");
+        //}else if(per <=89  && per >= 80)
+        //{
+        //    Console.WriteLine("Total Marks: " + total);
+        //    Console.WriteLine("Obtained Marks: " + obt);
+        //    Console.WriteLine("Grade A");
+        //    Console.WriteLine("You Persentage is " + per + "%");
+        //}else if(per <=79 && per >= 70)
+        //{
+        //    Console.WriteLine("Total Marks: " + total);
+        //    Console.WriteLine("Obtained Marks: " + obt);
+        //    Console.WriteLine("Grade B");
+        //    Console.WriteLine("You Persentage is " + per + "%");
+        //}
+        //else if (per <= 69 && per >= 60)
+        //{
+        //    Console.WriteLine("Total Marks: " + total);
+        //    Console.WriteLine("Obtained Marks: " +obt);
+        //    Console.WriteLine("Grade C");
+        //    Console.WriteLine("You Persentage is " + per + "%");
+        //}
+        //else if (per <= 59 && per >= 50)
+        //{
+        //    Console.WriteLine("Total Marks: " + total);
+        //    Console.WriteLine("Obtained Marks: " +obt);
+        //    Console.WriteLine("Grade D");
+        //    Console.WriteLine("You Persentage is " + per + "%");
+        //}
+        //else if (per <= 49 && per >= 35)
+        //{
+        //    Console.WriteLine("Total Marks: "+ total);
+        //    Console.WriteLine("Obtained Marks: " + obt);
+        //    Console.WriteLine("Grade Pass");
+        //    Console.WriteLine("You Persentage is " + per + "%");
+        //}
+        //else if (per <= 34 && per >= 0)
+        //{
+        //    Console.WriteLine("Total Marks: "+ total);
+        //    Console.WriteLine("Obtained Marks: " + obt);
+        //    Console.WriteLine("Grade Fail");
+        //    Console.WriteLine("You Persentage is " + per + "%");
+        //}
+
+        //Week days in Switch Case
+
+        //Console.Write("Enter number ");
+        //int num=Convert.ToInt32(Console.ReadLine());
+        //switch (num)
+        //{
+        //    case 1:
+        //        Console.WriteLine("Satur day");
+        //        break;
+        //        case 2:
+        //        Console.WriteLine("Sunday");
+        //        break;
+        //        case 3:
+        //        Console.WriteLine("Monday");
+        //        break;
+
+        //}
+
+    }
+    }
